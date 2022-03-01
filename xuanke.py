0962c49dbba6270cdbd2e8aebd687e529cea69e0
xuyuanfan <120812312@qq.com>
2022-03-01 19:38:34 +0800

    update xuanke.py

--- xuanke.py
+++ xuanke.py
@@ -6,30 +6,13 @@ import time
 import re
 
 
-page_xnxq = ''
-username = ''  #fill in
-password = ''  #fill in
-type = '核心专业'
-var1 = ''
-var2 = ''
-var3 = ''
-if type == '一般专业':
-    var1 = 'xslbxk'
-    var2 = 'ZYL'
-    var3 = 'xslbxk'
-elif type == '核心通识':
-    var1 = 'xsxk'
-    var2 = 'TSL'
-    var3 = 'tsk'
-elif type == '一般通识':
-    var1 = 'xsxk'
-    var2 = 'TSL'
-    var3 = 'qxrx'
-elif type == '核心专业':
-    var1 = 'xslbxk'
-    var2 = 'ZYL'
-    var3 = 'xslbxk'
-
+username = ''                    #帐号
+password = ''                  #密码
+type = '一般专业'                        #选课类型（一般专业课 核心专业课 核心通识课 一般专业课）
+typeDict = {'一般专业':('xslbxk','ZYL','xslbxk'), '核心专业':('xslbxk', 'ZYL', 'xslbxk'), '核心通识':('xsxk','TSL','tsk'),'一般通识':('xsxk','TSL','qxrx')}
+var1,var2,var3 = typeDict[type]
+course = 'B3J063220'                    #课程编号  B3J063220为举例
+rwh = '2021-2022-2-' + course + '-001'  #001为默认 同一课程编号有不同老师时改为00n n为搜索该课出现的第n个
 jiaowu_url = 'http://jwxt.buaa.edu.cn:8080/ieas2.1/'
 login_url = 'https://sso.buaa.edu.cn/login?service=' \
             + quote(jiaowu_url, 'utf-8') + 'welcome'
@@ -58,7 +41,6 @@ def login(username, password, session):
 
 
 def _get_hidden_items(text):
-    ''' get hidden item of html text '''
     item_pattern = re.compile(r'<input type="hidden" id="(.*?)" name="(.*?)"\s+value="(.*?)"')
     items = re.findall(item_pattern, text)
     return {item[1]: item[2] for item in items}
@@ -70,8 +52,6 @@ def chose():
     list_url = 'http://jwxt.buaa.edu.cn:8080/ieas2.1/xslbxk/queryXsxkList?pageXkmkdm=' + var2
     response = session.post(list_url,allow_redirects=True)
     payload=_get_hidden_items(response.text)
-    course = 'B3I063130' #to modify
-    rwh = '2021-2022-2-' + course + '-001'
     payload['pageXnxq'] = '2021-20222'
     payload['pageKkxiaoqu'] = ''
     payload['pageKkyx'] = ''
@@ -92,4 +72,4 @@ def chose():
 
 while True:
     chose()
-    time.sleep(5)
+    time.sleep(1)
